d1b10be74b51a4882bb53b216b329432f6486313
diegocfreire@gmail.com <10pista100nha>
2017-02-03 11:50:46 -0300

    Refatorando aplicação.

---
+++ config.py
@@ -0,0 +1,7 @@
+__author__ = 'diego.freire'
+
+user_name = 'seu_email@gmail.com'
+user_pass = 'sua_senha'
+attach_dir = 'c:/tmp'
+imap_url = 'imap.gmail.com'
+imap_port = 993
--- readGmail.py
+++ readGmail.py
@@ -1,54 +1,59 @@
 #!/usr/bin/python
-__author__ = "Diego Freire"
+__author__ = 'Diego Freire'
+
 import email
 import imaplib
 import os
+import config
 
-mail = imaplib.IMAP4_SSL('imap.gmail.com', 993)
-u_name = 'seu_email@gmail.com'
-u_pass = 'sua_senha'
-d_dir = 'c:/tmp'
 
 def read_mensages():
     try:
         mail.select('Inbox')
-
-        #Busca mensagens nao lidas
+        # Busca mensagens nao lidas
         (return_code, messages) = mail.search(None, '(UNSEEN)')
-        
+
         if return_code == 'OK':
             for num in messages[0].split():
                 typ, data = mail.fetch(num, '(RFC822)')
-
                 # Salvando anexos
-                m = email.message_from_string(data[0][1])
-                for part in m.walk():
-                    filename = part.get_filename()
-                    if filename is not None:
-                        sv_path = os.path.join(d_dir, filename)
-                        if not os.path.isfile(sv_path):
-                            print sv_path
-                            fp = open(sv_path, 'wb')
-                            fp.write(part.get_payload(decode=True))
-                            fp.close()
+                save_attachments(data[0][1])
+                # Imprimindo algumas propriedades do header para testes
+                print_properties(data)
 
-                # Imprimindo algumas propriedades do header
-                for response_part in data:
-                    if isinstance(response_part, tuple):
-                        original = email.message_from_string(response_part[1])
-                        data = response_part.get
-                        print original['From']
-                        print original['Subject']
-                        print original['Body']
-        print "Procurando mensagens..."
-    except Exception,e:
-        print "Erro Mensagem: "+str(e)
+        print 'Procurando mensagens...'
+    except Exception, e:
+        print 'Erro Mensagem: ' + str(e)
+
+
+def save_attachments(data):
+    m = email.message_from_string(data)
+    for part in m.walk():
+        filename = part.get_filename()
+        if filename is not None:
+            sv_path = os.path.join(config.attach_dir, filename)
+            if not os.path.isfile(sv_path):
+                print sv_path
+                fp = open(sv_path, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+
+
+def print_properties(data):
+    for response_part in data:
+        if isinstance(response_part, tuple):
+            original = email.message_from_string(response_part[1])
+            print original['From']
+            print original['Subject']
+            print original['Body']
+
 
 # Efetua conexao na conta de email.
 try:
-    mail.login(u_name, u_pass)
+    mail = imaplib.IMAP4_SSL(config.imap_url, config.imap_port)
+    mail.login(config.user_name, config.user_pass)
 except Exception, e:
-    print "Erro Login: "+str(e)
+    print 'Erro Login: ' + str(e)
 
 # Inicia procura de mensagens nao lidas.
 while True:
